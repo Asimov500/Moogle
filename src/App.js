dce5fd4a38748b639412aee32a5332cd40f0984c
Asimov500 <asimov@dsl.pipex.com>
2020-03-23 23:17:42 +0000

    Initial Home Page Set up
    Initial Home Page Set up. Next we need to set up the page routing. There may only be two pages, but there could be three if I add an about page.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import AutoCompleteText from "./components/AutoCompleteText";
 import './App.css';
+import cowList from './data/cowList'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    constructor(props){
+        super(props);
+    };
+
+    render(){
+        return(
+            <div>
+                <div className='home'>
+                    <div>
+                        <img alt="Moogle" src="images/moogle.svg"/>
+                    </div>
+                    <div className="App-Component">
+                        <AutoCompleteText  cowList={cowList} triggerParentUpdate={this.setLayout} />
+                    </div>
+                </div>
+            </div>
+        )
+    };
 }
 
 export default App;
---
+++ src/components/AutoCompleteText.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import './AutoCompleteText.css';
+
+export default class AutoCompleteText extends React.Component{
+	constructor(props){
+		super(props);
+
+		this.state={
+			suggestions:[],
+			text:'',
+			filteredList:[]
+		}
+	}
+
+	onTextChanged = (e) => {
+		const value = e.target.value;
+		let suggestions = [];
+		if(value.length > 0){
+		//	const regex = new RegExp(`^${value}`,'i');
+			suggestions = this.props.cowList.sort().filter(h => h.name.toLowerCase().includes(value.toLowerCase()));
+		}
+		this.setState(() => ({ suggestions, text: value }));
+	};
+
+	OnEnterKey = (e) => {
+		const value = e.target.value;
+		let suggestions = [];
+		if (e.key === 'Enter') {
+			if(value.length > 0) {
+				//	console.log(this.props.cowList);
+				suggestions = this.props.cowList.sort().filter(h => h.name.toLowerCase().includes(value.toLowerCase()));
+				console.log(suggestions);
+				//alert(value);
+
+				this.setState(() => ({suggestions, text: value}));
+				this.setState(() => ({
+					text: value.name,
+					suggestions: []
+				}));
+
+				this.props.triggerParentUpdate(suggestions);
+			}
+		}
+
+
+	};
+
+	suggestionSelected(value){
+		this.setState(() => ({
+			text: value.name,
+			suggestions:[]
+		}));
+		let arrayConvert =[];
+		arrayConvert.push(value);
+		this.props.triggerParentUpdate(arrayConvert);
+	}
+
+	renderSuggstions () {
+		const { suggestions } = this.state;
+		if (suggestions.length === 0){
+			return null;
+		}
+		return (
+			<ul>
+				{suggestions.map((item => <li onClick={() => this.suggestionSelected(item)}>{item.name}</li>))}
+			</ul>
+		);
+	}
+
+	render() {
+		const { text } = this.state;
+		return (
+			<div className="AutoCompleteText">
+				<input value={text} onChange={this.onTextChanged} onKeyDown={this.OnEnterKey} type="text" />
+				<ul>
+					{this.renderSuggstions()}
+				</ul>
+			</div>
+		);
+	};
+
+
+}

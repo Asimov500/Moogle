19bd89b3191eb873efaa9eddb58dc84c7e0251d4
Asimov500 <asimov@dsl.pipex.com>
2020-03-24 01:48:05 +0000

    Home page style looking fine but...
    Home page style is looking fine but must get the redirect to the search page working next.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,45 @@
 import React,{Component} from 'react';
-//import '/../AutoCompleteText.css';
+import logo from '../images/moogle.svg';
+import AutoCompleteText from "../components/AutoCompleteText";
+import cowList from '../data/cowList'
+import '../App.css';
+import {Redirect} from 'react-router-dom';
 
 export default class Home extends Component{
 	constructor(props){
 		super(props);
 	}
 
+	gotoSearch = (cowListFiltered) =>{
+
+		alert('Search Page');
+		/*
+		this.setState({
+			layout:'appLayout2',
+			showLines:true,
+			cowListFiltered: cowListFiltered
+		});*/
+	};
+
+
 	render() {
+
+/*
+	 return (
+		<Redirect push to="/search" />
+	 );
+*/
+
+
+
 		return (
-			<div className="AutoCompleteText">
-				<input onChange={this.onTextChanged} onKeyDown={this.OnEnterKey} type="text" />
+			<div className="home">
+				<div>
+					<img src={logo} alt="Moogle" />
+				</div>
+				<div className="App-Component">
+					<AutoCompleteText  cowList={cowList} triggerParentUpdate={this.gotoSearch} />
+				</div>
 
 			</div>
 		);

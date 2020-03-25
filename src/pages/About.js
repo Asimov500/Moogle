31e32838379f738d6b236fcd4425f2d6e69b49c2
Asimov500 <asimov@dsl.pipex.com>
2020-03-25 20:19:32 +0000

    Still working on about page
    Got place holder text in at the moment, but needs a bit more work to make it look better

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,15 @@
 import React,{Component} from 'react';
+import logo from "../images/moogle.svg";
+import brad from "../images/brad.jpg";
+import AutoCompleteText from "../components/AutoCompleteText";
+import cowList from "../data/cowList";
 //import './AutoCompleteText.css';
+
+
+
+const imgStyle = {
+	//width:700
+};
 
 export default class About extends Component{
 	/*constructor(props){
@@ -7,8 +17,32 @@ export default class About extends Component{
 	}*/
 	render() {
 		return (
-			<div className="AutoCompleteText">
-				<h1>About</h1>
+			<div>
+			<div className="search">
+				<div>
+					<img src={logo} alt="Moogle" />
+				</div>
+				<div className="App-Component">
+					hello
+				</div>
+			</div>
+			<div className="App-Response">
+				<ul>
+					<li>
+						<div className="bodText">
+							<h3>About</h3>
+							<div>
+								<div className="imfloat"><img  src={brad} alt="Moogle" /></div>
+								Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+							</div>
+						</div>
+						<div className="imDiv">
+
+						</div>
+					</li>
+				</ul>
+			</div>
 			</div>
 		);
 	};
